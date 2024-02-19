3aea8da877b4df016dba77d337ceec198fd2a6c5
IgorKozarchuk <igor.a.kozarchuk@gmail.com>
2024-02-19 17:07:12 +0200

    correlation

--- 02_correlation_check.py
+++ 02_correlation_check.py
@@ -1,25 +1,40 @@
 import pandas as pd
+import seaborn as sns
 from matplotlib import pyplot as plt
+from scipy.stats import pearsonr
 
 
 df = pd.read_csv("AESUM_clean.csv")
 print(df.head())
+print()
 
-x1 = df[["Довжина"]]
-x2 = df[["Категорія"]]
-x3 = df[["Обл"]]
-x4 = df[["ВікБуд"]]
-x5 = df[["ВікРем"]]
+# Correlation matrix
+print("Correlation:")
+print(df.corr())
+print()
 
-y = df[["Стан"]]
+plt.figure(figsize=(8, 6))
+plt.title("Correlation matrix")
+sns.heatmap(df.corr())
+plt.show()
 
-plt.scatter(x1, y)
-plt.show()
-plt.scatter(x2, y)
-plt.show()
-plt.scatter(x3, y)
-plt.show()
-plt.scatter(x4, y)
-plt.show()
-plt.scatter(x5, y)
-plt.show()
+# Print Pearson's correlation (sorted, not taking into account sign) - same result as df.corr()
+def get_pearson_sorted(df):
+	head = {"Feature": "Pearson correlation coefficient"}
+	result = {}
+
+	for x in df.columns[:-1]:
+		p_corr = pearsonr(df[x], df[df.columns[-1]])[0] # current and last cols
+		result[x] = p_corr
+	
+	# sorted dict
+	sorted_result = {k: v for k, v in sorted(result.items(), key=lambda item: abs(item[1]), reverse=True)}
+
+	# print
+	print("Feature | Pearson's coefficient")
+	for k, v in sorted_result.items():
+		print(f"{k}: {v}")
+
+	return sorted_result
+
+get_pearson_sorted(df)
