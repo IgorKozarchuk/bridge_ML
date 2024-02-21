0f3f867cda7b746cea4af629b97de06ecba8248f
IgorKozarchuk <igor.a.kozarchuk@gmail.com>
2024-02-21 14:11:27 +0200

    decision tree

---
+++ 05_decision_tree.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+
+
+df = pd.read_csv("AESUM_clean.csv")
+# print(df.head())
+
+# Features
+X = df[["Довжина", "Категорія", "Обл", "ВікБуд"]]
+# Target
+y = df[["Стан"]]
+
+# Train/test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Decision tree model
+dtree_model = tree.DecisionTreeClassifier()
+
+# Train the model
+dtree_model.fit(X_train, y_train)
+
+# Score 
+print(dtree_model.score(X_test, y_test))
+print()
+
+# Predict
+y_pred = dtree_model.predict(X_test)
+
+# Metrics
+print("Classification report:")
+print(classification_report(y_test, y_pred))
+print()
+
+# Confusion matrix
+c_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion matrix:")
+print(c_matrix)
+print()
+
+# Plot confusion matrix
+disp = ConfusionMatrixDisplay(confusion_matrix=c_matrix, display_labels=dtree_model.classes_)
+disp.plot(cmap="magma_r")
+disp.ax_.set_title("Confusion matrix")
+plt.show()
